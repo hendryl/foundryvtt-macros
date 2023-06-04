a92d6089bb804566aa27195d0647c92f9b3ba0fc
hendry <hendry@Aether>
2023-06-04 23:21:21 +0700

    Update ca calculation logic

--- create-sticky-note.js
+++ create-sticky-note.js
@@ -1,13 +1,17 @@
 const content = `
   <div style="padding-bottom: 1rem;>
-    <label for="message">Text: </label>
-    <textarea id="create-sticky-note-input" style="width: 100%;" name="message"></textarea>
+    <form>
+        <label for="message">Text: </label>
+        <input id="create-sticky-note-input" style="height: 2rem; width: 100%;" name="message">
+    </form>
   </div>
 `
-;
+
+// Callback to create note
 async function createNote(message) {
   let maxWordLength = 0
   
+  // logic to calculate proper square size based on text length (not perfect!)
   if (message.split(' ').length <= 2 && message.length < 16) {
     maxWordLength = message.length
   } else {
@@ -15,46 +19,59 @@ async function createNote(message) {
     .sort((a , b) => (a.length - b.length) * -1)[0]
     .length
 
-    maxWordLength = maxWordLength < 8 ? 8 : maxWordLength;
+    // Make sure text should be around 8 lines
+    while (Math.round(message.length / maxWordLength) > 10) {
+        maxWordLength = maxWordLength + 0.5;
+    }
   }
 
+  // 29.5 is good width for Signika font
   const size = maxWordLength * 29.5
 
+  if (canvas.scene.width / 2 < size) {
+    ui.notifications.error(`Sticky Note too large!`)
+    return
+  }
+
+  // make the drawing
   const myDrawing = await canvas.scene.createEmbeddedDocuments("Drawing", [{
     type: CONST.DRAWING_TYPES.RECTANGLE,
-    x: canvas.scene.width - size - 5,
-    y: canvas.scene.height - size - 5,
     strokeWidth: 8,
-    width: size,
-    height: size,
     fillType: CONST.DRAWING_FILL_TYPES.SOLID,
     fillColor: '#fac800',
     fillAlpha: 1,
     text: message,
+    // width == height to make a square
+    width: size,
+    height: size,
+    // put in bottom right of scene
+    x: canvas.scene.width - size - 5,
+    y: canvas.scene.height - size - 5,
   }]);
-
-  window.myDrawing = myDrawing
 
   ui.notifications.info(`Sticky Note Created!`)
 }
 
+// Create and render the dialog box
 const noteInputDialog = new Dialog({
-  title: "Create Sticky Note",
+  title: 'Create Sticky Note',
   content: content,
   buttons: {
-    button1: {
-      label: "Cancel",
+    cancel: {
+      label: 'Cancel',
       callback: null,
       icon: `<i class="fas fa-times"></i>`
     },
-    button2: {
-      label: "Submit",
+    submit: {
+      label: 'Submit',
       callback: () => createNote(document.getElementById('create-sticky-note-input').value),
       icon: `<i class="fas fa-check"></i>`
     },
-  }
+  },
+  default: 'submit'
 }).render(true)
 
+// Wait until render is done to focus on the textfield
 setTimeout(() => {
   document.getElementById('create-sticky-note-input').focus();
 }, 0)
